3cf371239726ee7a2d60d15bb57a8c336f3e4e6a
abrie <abrie@users.noreply.github.com>
2020-06-14 21:10:20 -0400

    Inline eventhandler in constructor calls.

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -11,73 +11,53 @@ import { pathProcessor } from "./pathprocessor";
 import { TurtleEventHandler } from "./turtle";
 import LSystem from "./lsystem";
 
-const store = new Store();
-
-document
-  .getElementById("zoom")
-  .addEventListener(
-    "input",
-    (evt) => (canvas.zoom = parseFloat((evt.target as HTMLInputElement).value))
-  );
-
-document
-  .getElementById("send-button")
-  .addEventListener("click", () => store.store(canvas.svg));
-
-const messagesEventHandler: MessagesEventHandler = {
+const messages = new Messages(<MessagesEventHandler>{
   onOpen: () => console.log("connection open"),
   onClose: () => console.log("connection closed"),
   onError: () => console.log("connection error"),
   onMessage: (message) => processMessage(message),
-};
+});
 
-const messages = new Messages(messagesEventHandler);
-
-const cursorTrackerEventHandler: CursorTrackerEventHandler = {
+const cursors = new CursorTracker(<CursorTrackerEventHandler>{
   onNewCursor: (cursor) => canvas.addCursor(cursor),
   onDeadCursor: (cursor) => canvas.removeCursor(cursor),
-};
+});
 
-const cursors = new CursorTracker(cursorTrackerEventHandler);
-
-const polylineEventHandler: PolylineEventHandler = {
+const polylines = new Polylines(<PolylineEventHandler>{
   onNewPolyline: (polyline) => canvas.startPolyline(polyline),
   onFinishedPolyline: (polyline) => canvas.finishPolyline(polyline),
   onCanceledPolyline: (polyline) => canvas.cancelPolyline(polyline),
-};
+});
 
-const polylines = new Polylines(polylineEventHandler);
-
-const pathTrackerEventHandler: PathTrackerEventHandler = {
-  onNewPath: ({ id, data }: AttributedCoordinates) => {
-    polylines.startPolyline({ id, data });
+const pathTracker = new PathTracker(
+  <PathTrackerEventHandler>{
+    onNewPath: ({ id, data }: AttributedCoordinates) => {
+      polylines.startPolyline({ id, data });
+    },
+    onFinishedPath: ({ id, data }: AttributedCoordinates) => {
+      polylines.finishPolyline({ id, data });
+    },
+    onUpdatedPath: ({ id, data }: AttributedCoordinates) => {
+      polylines.updatePolyline({ id, data });
+    },
+    onCanceledPath: ({ id }: AttributedCoordinates) => {
+      polylines.cancelPolyline({ id });
+    },
   },
-  onFinishedPath: ({ id, data }: AttributedCoordinates) => {
-    polylines.finishPolyline({ id, data });
-  },
-  onUpdatedPath: ({ id, data }: AttributedCoordinates) => {
-    polylines.updatePolyline({ id, data });
-  },
-  onCanceledPath: ({ id }: AttributedCoordinates) => {
-    polylines.cancelPolyline({ id });
-  },
-};
-
-const pathTracker = new PathTracker(pathTrackerEventHandler, pathProcessor);
-
-const pointerEventHandler: PointerEventHandler = {
-  onPointerDown: (a: AttributedCoordinate) => penTracker.down(a),
-  onPointerUp: (a: AttributedCoordinate) => penTracker.up(a),
-  onPointerMove: (a: AttributedCoordinate) => penTracker.move(a),
-  onPointerCancel: (a: AttributedCoordinate) => penTracker.cancel(a),
-};
+  pathProcessor
+);
 
 const canvas = new Canvas({
   target: document.getElementById("canvas"),
-  pointerEventHandler,
+  pointerEventHandler: <PointerEventHandler>{
+    onPointerDown: (a: AttributedCoordinate) => penTracker.down(a),
+    onPointerUp: (a: AttributedCoordinate) => penTracker.up(a),
+    onPointerMove: (a: AttributedCoordinate) => penTracker.move(a),
+    onPointerCancel: (a: AttributedCoordinate) => penTracker.cancel(a),
+  },
 });
 
-const penTrackerEventHandler: PenTrackerEventHandler = {
+const penTracker = new PenTracker(<PenTrackerEventHandler>{
   onPenDown: (a: AttributedCoordinate) => {
     pathTracker.startPath(a);
     messages.send({
@@ -114,9 +94,13 @@ const penTrackerEventHandler: PenTrackerEventHandler = {
     pathTracker.cancelPath(a);
     messages.send({ action: "cancel", ...a });
   },
-};
+});
 
-const penTracker = new PenTracker(penTrackerEventHandler);
+const lsystem = new LSystem(<TurtleEventHandler>{
+  onTurtleDown: (a: AttributedCoordinate) => penTracker.down(a),
+  onTurtleMove: (a: AttributedCoordinate) => penTracker.move(a),
+  onTurtleUp: (a: AttributedCoordinate) => penTracker.up(a),
+});
 
 function processMessage({ clientId, action, id, data }: Message) {
   const attributedCoordinate: AttributedCoordinate = {
